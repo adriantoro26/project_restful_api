678750161640136326bb992f00afc445b52944eb
Adrian Toro <adriandesantis26@gmail.com>
2021-06-05 20:47:19 -0400

    First endpoint created

--- src/restful_api/restful_api_app/views.py
+++ src/restful_api/restful_api_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class helloApiView(APIView):
+
+   def get(self, request, format=None):
+
+      an_apiview = [
+         'This is a test', 
+         'We are using HTTP get method',
+         'This data comes from views file using APIVIEWS'
+      ]
+
+      return Response({
+         'message': 'Hello from views file',
+         'data':an_apiview
+      })
