aa85470c32c6829d6e1e6efe05e0148e500937b5
Adrian Toro <adriandesantis26@gmail.com>
2021-06-07 15:28:32 -0400

    User login added

--- src/restful_api/restful_api_app/urls.py
+++ src/restful_api/restful_api_app/urls.py
@@ -8,6 +8,8 @@ router.register(r'hello-viewset', views.HelloViewSet, basename = 'hello-viewset'
 
 # When using the model viewset we don't need to specify the basename
 router.register(r'profile', views.UserViewSet)
+# Login. Basename is required because it is not a model.
+router.register(r'login', views.LoginViewSet, basename = 'login')
 
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view()),
